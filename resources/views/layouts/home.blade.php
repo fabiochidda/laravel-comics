840583f544ab57ea7cecbf55a0bdb857957dacdb
fabiochidda <fabio.chidda99@gmail.com>
2022-04-21 03:43:55 +0200

    Aggiunto l'header

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Laravel Comics</title>
 </head>
 <body>
 
     <header>
-
+        @include('partials.header')
     </header>
 
     <main>
@@ -17,7 +18,7 @@
     </main>
 
     <footer>
-        
+
     </footer>
     
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,66 @@
+@php
+    
+$navLinks = [
+    [
+        'text' => 'CHARACTERS',
+        'href' => '#',
+        'isActive' => true,
+    ],
+    [
+        'text' => 'COMICS',
+        'href' => '#',
+    ],
+    [
+        'text' => 'MOVIES',
+        'href' => '#'
+    ],
+    [
+        'text' => 'TV',
+        'href' => '#'
+    ],
+    [
+        'text' => 'GAMES',
+        'href' => '#'
+    ],
+    [
+        'text' => 'COLLECTIBLES',
+        'href' => '#'
+    ],
+    [
+        'text' => 'VIDEOS',
+        'href' => '#'
+    ],
+    [
+        'text' => 'FANS',
+        'href' => '#'
+    ],
+    [
+        'text' => 'NEWS',
+        'href' => '#'
+    ],
+    [
+        'text' => 'SHOP',
+        'href' => '#'
+    ],
+]; 
+        
+    
+@endphp
+
+
+<div class="container">
+    <div class="container-lg">
+        <figure class="header-logo">
+            <img src="{{'img/dc-logo.png'}}" alt="">
+        </figure>
+        <nav class="nav-container">
+            <ul class="nav">
+                @foreach ($navLinks as $el)
+                    <li>
+                        <a :href="{{ $el['href'] }}">{{ $el['text'] }}</a>
+                    </li>
+                @endforeach     
+            </ul>
+        </nav>
+    </div>
+</div>
